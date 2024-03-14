25cb14832401399ba48d79792e452071f5bfefbf
Kacper Szady <83802@student.ue.poznan.pl>
2024-03-14 09:14:32 +0100

    Dodanie funkcji na pole trojkata

--- excercises/exercises1.py
+++ excercises/exercises1.py
@@ -1,3 +1,10 @@
+import math
+
 bok_a = 10.5
 pole_kw = bok_a**2
 print("Pole kwadratu wynosi: "+str(pole_kw))
+
+podstawa = 40
+wysokosc = 12
+pole_troj = (podstawa*wysokosc)/2
+print("Pole trojkata wynosi: "+str(pole_troj))
