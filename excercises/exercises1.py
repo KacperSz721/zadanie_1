5c10a990be1809bedc8f49cab3d576a025fc2e36
Kacper Szady <83802@student.ue.poznan.pl>
2024-03-14 09:17:40 +0100

    Dodanie funkcji na pole trapezu

--- excercises/exercises1.py
+++ excercises/exercises1.py
@@ -8,3 +8,9 @@ podstawa = 40
 wysokosc = 12
 pole_troj = (podstawa*wysokosc)/2
 print("Pole trojkata wynosi: "+str(pole_troj))
+
+a = 20
+b = 13
+h = 6
+pole_trap = ((a+b)*h)/2
+print("Pole trapezu wynosi: "+str(pole_trap))
